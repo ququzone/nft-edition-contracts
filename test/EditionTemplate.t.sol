d0fca60a00d07ec8b548a35c4943af84970ef8a1
ququzone <xueping.yang@gmail.com>
2023-05-30 20:30:20 +0800

    initial project

---
+++ test/EditionTemplate.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/EditionTemplate.sol";
+
+contract EditionTemplateTest is Test {
+    EditionTemplate public tempalte;
+
+    function setUp() public {
+    }
+}
