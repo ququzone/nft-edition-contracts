cc302cc20e01c859d9e61a7d9c550bf7a03c2314
ququzone <xueping.yang@gmail.com>
2023-06-08 11:02:08 +0800

    add create edition method

--- src/NFTCreatorV1.sol
+++ src/NFTCreatorV1.sol
@@ -3,7 +3,9 @@ pragma solidity ^0.8.13;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IMetadataRenderer} from "./interfaces/IMetadataRenderer.sol";
-import {ERC721Drop} from "./ERC721Drop.sol";
+import {EditionMetadataRenderer} from "./metadata/EditionMetadataRenderer.sol";
+import {DropMetadataRenderer} from "./metadata/DropMetadataRenderer.sol";
+import {IERC721Drop, ERC721Drop} from "./ERC721Drop.sol";
 
 contract NFTCreatorV1 is Ownable {
     event CreatedDrop(address indexed creator, address indexed editionContractAddress, uint256 editionSize);
@@ -11,9 +13,19 @@ contract NFTCreatorV1 is Ownable {
     uint256 public mintFee;
     address payable public mintFeeRecipient;
 
-    constructor(uint256 _mintFee, address payable _mintFeeRecipient) {
+    EditionMetadataRenderer public immutable editionMetadataRenderer;
+    DropMetadataRenderer public immutable dropMetadataRenderer;
+
+    constructor(
+        uint256 _mintFee,
+        address payable _mintFeeRecipient,
+        EditionMetadataRenderer _editionMetadataRenderer,
+        DropMetadataRenderer _dropMetadataRenderer
+    ) {
         mintFee = _mintFee;
         mintFeeRecipient = _mintFeeRecipient;
+        editionMetadataRenderer = _editionMetadataRenderer;
+        dropMetadataRenderer = _dropMetadataRenderer;
     }
 
     function setMintFee(uint256 _mintFee) external onlyOwner {
@@ -25,7 +37,6 @@ contract NFTCreatorV1 is Ownable {
     }
 
     function createAndConfigureDrop(
-        IMetadataRenderer metadataRenderer,
         string memory name,
         string memory symbol,
         address defaultAdmin,
@@ -33,8 +44,9 @@ contract NFTCreatorV1 is Ownable {
         uint16 royaltyBPS,
         address payable fundsRecipient,
         bytes[] memory setupCalls,
+        IMetadataRenderer metadataRenderer,
         bytes memory metadataInitializer
-    ) public returns (ERC721Drop) {
+    ) public returns (address payable) {
         ERC721Drop drop = new ERC721Drop(mintFee, mintFeeRecipient, name, symbol, defaultAdmin, fundsRecipient);
         drop.initConfig({
             _setupCalls: setupCalls,
@@ -43,6 +55,99 @@ contract NFTCreatorV1 is Ownable {
             _metadataRenderer: metadataRenderer,
             _metadataRendererInit: metadataInitializer
         });
-        return drop;
+        return payable(address(drop));
+    }
+
+    function setupDropsContract(
+        string memory name,
+        string memory symbol,
+        address defaultAdmin,
+        address payable fundsRecipient,
+        uint64 editionSize,
+        uint16 royaltyBPS,
+        IERC721Drop.SalesConfiguration memory saleConfig,
+        IMetadataRenderer metadataRenderer,
+        bytes memory metadataInitializer
+    ) public returns (address) {
+        bytes[] memory setupData = new bytes[](1);
+        setupData[0] = abi.encodeWithSelector(
+            ERC721Drop.setSaleConfiguration.selector,
+            saleConfig.publicSalePrice,
+            saleConfig.maxSalePurchasePerAddress,
+            saleConfig.publicSaleStart,
+            saleConfig.publicSaleEnd,
+            saleConfig.presaleStart,
+            saleConfig.presaleEnd,
+            saleConfig.presaleMerkleRoot
+        );
+        address newDropAddress = createAndConfigureDrop({
+            name: name,
+            symbol: symbol,
+            defaultAdmin: defaultAdmin,
+            fundsRecipient: fundsRecipient,
+            editionSize: editionSize,
+            royaltyBPS: royaltyBPS,
+            setupCalls: setupData,
+            metadataRenderer: metadataRenderer,
+            metadataInitializer: metadataInitializer
+        });
+
+        emit CreatedDrop({creator: msg.sender, editionSize: editionSize, editionContractAddress: newDropAddress});
+
+        return newDropAddress;
+    }
+
+    function createDrop(
+        string memory name,
+        string memory symbol,
+        address defaultAdmin,
+        uint64 editionSize,
+        uint16 royaltyBPS,
+        address payable fundsRecipient,
+        IERC721Drop.SalesConfiguration memory saleConfig,
+        string memory metadataURIBase,
+        string memory metadataContractURI
+    ) external returns (address) {
+        bytes memory metadataInitializer = abi.encode(metadataURIBase, metadataContractURI);
+        return
+            setupDropsContract({
+                defaultAdmin: defaultAdmin,
+                name: name,
+                symbol: symbol,
+                royaltyBPS: royaltyBPS,
+                editionSize: editionSize,
+                fundsRecipient: fundsRecipient,
+                saleConfig: saleConfig,
+                metadataRenderer: dropMetadataRenderer,
+                metadataInitializer: metadataInitializer
+            });
+    }
+
+    function createEdition(
+        string memory name,
+        string memory symbol,
+        uint64 editionSize,
+        uint16 royaltyBPS,
+        address payable fundsRecipient,
+        address defaultAdmin,
+        IERC721Drop.SalesConfiguration memory saleConfig,
+        string memory description,
+        string memory animationURI,
+        string memory imageURI
+    ) external returns (address) {
+        bytes memory metadataInitializer = abi.encode(description, imageURI, animationURI);
+
+        return
+            setupDropsContract({
+                name: name,
+                symbol: symbol,
+                defaultAdmin: defaultAdmin,
+                editionSize: editionSize,
+                royaltyBPS: royaltyBPS,
+                saleConfig: saleConfig,
+                fundsRecipient: fundsRecipient,
+                metadataRenderer: editionMetadataRenderer,
+                metadataInitializer: metadataInitializer
+            });
     }
 }
